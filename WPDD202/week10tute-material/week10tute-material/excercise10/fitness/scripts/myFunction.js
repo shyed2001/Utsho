bf48e6bdaa57c7285b4a2c6200ac6d0f54700d4e
Shyed Shahriar <shyed2001@yahoo.com>
2024-05-30 02:06:54 +0600

    ok
    ok

---
+++ WPDD202/week10tute-material/week10tute-material/excercise10/fitness/scripts/myFunction.js
@@ -0,0 +1,25 @@
+/* Wasik Gaus
+29/05/20024
+ My Function List */
+
+ 
+ // Thank you alert to insert a single line comment.
+function tyAlert() {  
+  alert("Thank you for your interest in our fitness club. We will contact you soon"); 
+}
+
+// Burpees video window to insert a comment.
+function burpees() { 
+  window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+}
+
+// Plank video window to insert a comment.
+function plank() {
+   window.open("scripts/plank.html", "_blank", "width=610, height=360");
+   }
+
+   // Mountain climber video window to insert a comment.
+function mtnClimber() { 
+    window.open("scripts/mtn.html", "_blank", "width=610, height=360");
+  }
+
